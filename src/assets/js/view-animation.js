f7989393d73368c1d0630a02bcb26eb971df007c
kingkongdevs <87683213+kingkongdevs@users.noreply.github.com>
2025-03-31 12:45:44 +1100

    mo animations

--- src/assets/js/view-animation.js
+++ src/assets/js/view-animation.js
@@ -2,20 +2,31 @@ export default function viewAnimation() {
     // Components loading animations
     let elementsToObserve = document.querySelectorAll('.view-animation');
 
-    // intersection observer not support on ie11
+    // Check for IE11 support
     let isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
 
-    elementsToObserve.forEach(function(el, index){
-        if(isIE11){
+    elementsToObserve.forEach(function (el) {
+        if (isIE11) {
             el.classList.add('animated');
-        }else{
+        } else {
             new IntersectionObserver(
                 function (entries) {
-                    if (entries[0].isIntersecting === true) {
-                        entries[0].target.classList.add('animated')
-                    }
+                    entries.forEach(entry => {
+                        if (entry.isIntersecting) {
+                            entry.target.classList.add('animated');
+                        } else {
+                            // Get element's bounding box
+                            let rect = entry.target.getBoundingClientRect();
+                            let viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+                            // Remove 'animated' only if exiting through the bottom
+                            if (rect.top >= viewportHeight) {
+                                entry.target.classList.remove('animated');
+                            }
+                        }
+                    });
                 },
-            { threshold: [.5] }
+                { threshold: 0.5 }
             ).observe(el);
         }
     });
